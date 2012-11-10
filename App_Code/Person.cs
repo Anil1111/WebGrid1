7d36901c4f8926d231e9a93881ebef38838a31ad
Martin Dobie <mdobie@sky.com>
2012-11-10 16:07:33 +0000

    Initial Commit

---
+++ App_Code/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Person
+{
+    public int ID { get; set; }
+    public string Forename { get; set; }
+    public string Surname { get; set; }
+    public string Gender { get; set; }
+    public DateTime DOB { get; set; }
+}
---
+++ Readme.md
@@ -0,0 +1,2 @@
+#WebGrid Example
+Source code for blog post: [WebMatrix Webgrid] (http://www.mdobie.co.uk/2012/11/10/webmatrix-webgrid/)
